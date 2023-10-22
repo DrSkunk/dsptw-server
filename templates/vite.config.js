071073d1e241226bd62050defd1f8e7c8eab24cd
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2023-10-22 12:56:30 +0200

    better setup for building templates

--- templates/vite.config.js
+++ templates/vite.config.js
@@ -1,18 +1,47 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { resolve } from "path";
+import fs from "fs";
+import { resolve, join } from "path";
 import { defineConfig } from "vite";
+import { directoryPlugin } from "vite-plugin-list-directory-contents";
+
+const htmlDir = resolve(__dirname, "src", "pages");
+
+const htmlFiles = fs
+  .readdirSync(htmlDir)
+  .filter((file) => file.endsWith(".html") && file !== "index.html")
+  .reduce((acc, file) => {
+    const name = file.replace(".html", "");
+    acc[name] = join(htmlDir, file);
+    return acc;
+  }, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    directoryPlugin({ baseDir: "src" }),
+    svelte(),
+    {
+      name: "remove-pages-dir-from-html-path",
+      enforce: "post",
+      generateBundle(_, bundle) {
+        const htmlFileInSrcFolderPattern = /^pages\/.*\.html$/;
+        for (const outputItem of Object.values(bundle)) {
+          if (!htmlFileInSrcFolderPattern.test(outputItem.fileName)) {
+            continue;
+          }
+          outputItem.fileName = outputItem.fileName.replace("pages/", "");
+        }
+      },
+    },
+  ],
   root: "src",
-  base: "./",
+  server: {
+    open: "pages/",
+  },
   build: {
     rollupOptions: {
-      input: {
-        drieZesNegen: resolve(__dirname, "src", "drie-zes-negen.html"),
-        players: resolve(__dirname, "src", "players.html"),
-      },
+      input: htmlFiles,
+      external: [join(htmlDir, "index.html")],
     },
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
